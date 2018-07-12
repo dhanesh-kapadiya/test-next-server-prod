f3a1b8a8cca63294861578ad365edf14e374af29
Dhanesh Kapadiya <dhanesh.kapadiya92@gmail.com>
2018-07-12 06:29:48 +0530

    add code

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default () => (
-  <ul>
-    <li><Link href='/b' as='/a'><a>a</a></Link></li>
-    <li><Link href='/a' as='/b'><a>b</a></Link></li>
-    <li>
-      <Link
-        href={{pathname: '/posts', query: { id: '2' }}}
-        as='/posts/2'
-      >
-        <a>post #2</a>
-      </Link>
-    </li>
-  </ul>
-)
+export default class Index extends React.Component {
+  constructor(props) {
+    super (props)
+
+    this.state = {
+      status: 'Click me!'
+    }
+  }
+
+  makeCall = () => {
+    fetch('/check')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ status: 'Got it ! Check console.' })
+      })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <ul>
+          <li><Link href='/b' as='/a'><a>aa</a></Link></li>
+          <li><Link href='/a' as='/b'><a>bb</a></Link></li>
+          <li>
+            <Link
+              href={{pathname: '/posts', query: { id: '2' }}}
+              as='/posts/2'
+            >
+              <a>post #2</a>
+            </Link>
+          </li>
+        </ul>
+        <div onClick={ this.makeCall }>{ this.state.status }</div>
+      </React.Fragment>
+    )
+  }
+}
